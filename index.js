14c2da754d98f9032b252f8005c7fb37bbb8622a
selinylmz <54477284+selinylmz@users.noreply.github.com>
2022-01-03 18:40:29 +0300

    arrow function

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ console.log(message);
 const unit_price=100; //birim fiyat
 const unit=3;//adet
 const currency_unit="TL"//para birimi
-
 const total_price=`Toplam fiyat ${unit_price*unit}${currency_unit} `;
-
 console.log(total_price);
 
 //Shorthand Property Names(Kisayol obje anahtari isimleri)
-
 const name="Mehmet";
 const ages=35;
-
-const user_object={name:name,ages:age};
-
-//const user_object={name,age};
-
-
+//const user_object={name:name,ages:age}; //eger degisken isimleri ayniysa shorthand yazilir.
+const user_object={name,age};
 console.log(user_object);
+
+//Arrow Functions 
+console.log(total(3,5)); 
+function total(number1,number2){ //function keywordu ile tanimlarsak usstte de cagirabilirz ama arrow function tanimlandiktan sonra cagirilir.
+    return number1+number2;
+}
+
+const totalArrow=(number1,number2)=>number1+number2;
+console.log(totalArrow(3,5));
