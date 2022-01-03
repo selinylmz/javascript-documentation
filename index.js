e33b0c854b052d2a21f9dcc42e644983ad21e122
selinylmz <54477284+selinylmz@users.noreply.github.com>
2022-01-03 18:09:51 +0300

    Shorthand Properties

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ const currency_unit="TL"//para birimi
 const total_price=`Toplam fiyat ${unit_price*unit}${currency_unit} `;
 
 console.log(total_price);
+
+//Shorthand Property Names(Kisayol obje anahtari isimleri)
+
+const name="Mehmet";
+const ages=35;
+
+const user_object={name:name,ages:age};
+
+//const user_object={name,age};
+
+
+console.log(user_object);
